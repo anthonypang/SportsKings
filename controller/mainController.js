e60e6fda003b600677559bb7494a5d083ba6b3b5
Anthony Pang <anthonypang99@gmail.com>
2022-04-22 15:58:15 -0400

    fixed register page

--- controller/mainController.js
+++ controller/mainController.js
@@ -2,36 +2,44 @@ var express = require('express');
 
 var router = express.Router();
 
-router.get('/', function (req,res){
-    if(req.session.userProfile){
-        res.render('index', {user:req.session.userProfile.user})
-    } else{
+router.get('/', function (req, res) {
+    if (req.session.userProfile) {
+        res.render('index', { user: req.session.userProfile.user })
+    } else {
         res.render('index')
     }
-   
+
 });
 
-router.get('/about', function (req,res){
-    if(req.session.userProfile){
-        res.render('about', {user:req.session.userProfile.user})
-    } else{
+router.get('/about', function (req, res) {
+    if (req.session.userProfile) {
+        res.render('about', { user: req.session.userProfile.user })
+    } else {
         res.render('about')
     }
 });
 
-router.get('/contact', function (req,res){
-    if(req.session.userProfile){
-        res.render('contact', {user:req.session.userProfile.user})
-    } else{
+router.get('/contact', function (req, res) {
+    if (req.session.userProfile) {
+        res.render('contact', { user: req.session.userProfile.user })
+    } else {
         res.render('contact')
     }
 });
 
-router.get('/newConnection', function(req, res){
-    if(req.session.userProfile){
-        res.render('newConnection', {user:req.session.userProfile.user})
-    } else{
+router.get('/newConnection', function (req, res) {
+    if (req.session.userProfile) {
+        res.render('newConnection', { user: req.session.userProfile.user })
+    } else {
         res.render('newConnection')
+    }
+});
+
+router.get('/register', function (req, res) {
+    if (req.session.userProfile) {
+        res.render('register', { user: req.session.userProfile.user })
+    } else {
+        res.render('register')
     }
 });
 
